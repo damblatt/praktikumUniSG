81ad5c337cd90f505cfe175191f826199b67c951
damblatt <dam.blatt@gmail.com>
2022-10-05 09:57:35 +0200

    Add project files.

---
+++ datumsformatierung/datumsformatierung/DateFormatter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("datumsformatierung.test")]
+
+namespace datumsformatierung
+{
+    internal class DateFormatter
+    {
+        public string GetFormattedDate(Date date, string format)
+        {
+            bool isValidDate = DateIsValid(date);
+            if (isValidDate)
+            {
+                format = format.ToLower();
+                if (format == "ch")
+                {
+                    return $"CH: {date.Day}.{date.Month}.{date.Year}";
+                }
+                else if (format == "us")
+                {
+                    return $"US: {date.Month}/{date.Day}/{date.Year}";
+                }
+                else if (format == "iso")
+                {
+                    return $"ISO: {date.Year}-{date.Month}-{date.Day}";
+                }
+                else { return "invalid format"; }
+            }
+            else { return "invalid date"; }
+        }
+
+        public bool DateIsValid(Date date)
+        {
+            if (date.Day > 31 || date.Day < 1)
+            {
+                return false;
+            }
+            else if (date.Month > 12 || date.Month < 1)
+            {
+                return false;
+            }
+            else if (date.Year < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
