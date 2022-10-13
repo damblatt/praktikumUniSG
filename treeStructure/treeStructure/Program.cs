fbe73bc63dd16214195f2829b9290636290e8c56
damblatt <dam.blatt@gmail.com>
2022-10-13 14:06:52 +0200

    fileContent gets split in newlines

--- treeStructure/treeStructure/Program.cs
+++ treeStructure/treeStructure/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace treeStructure
+﻿using System.Text;
+
+namespace treeStructure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            var fileContent = File.ReadAllText("Resources/structure.txt");
+
+            List<string> lol = fileContent.Split(Environment.NewLine).ToList(); // removes all newlines
+
+            string[,] bruh = new string[lol.Count, lol.Count];
+            int count = 0;
+            foreach (string line in lol)
+            {
+                bruh[count, 0] = line;
+                count++;
+                //Console.WriteLine(line);
+            }
         }
+
+        //private string[,] FillArray()
+        //{
+
+        //}
     }
 }
